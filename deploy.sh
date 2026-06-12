3a72e023921eb1bb47e4047e4b07d3ef69b4c889
Aman Shrivastava <shriaman93@gmail.com>
2026-06-13 02:13:47 +0530

    Adding deploy.sh file

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+cd core
+mvn clean package -DskipTests
+sudo cp target/backend.jar /var/www/funfriday/backend.jar
+sudo systemctl daemon-reload
+sudo systemctl restart arcade-backend
+cd ..
