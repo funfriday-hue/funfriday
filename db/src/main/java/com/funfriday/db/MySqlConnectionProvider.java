e9a5f4c81e7acd1d184ad30f7c1ced1764d5de12
Aman Shrivastava <shriaman93@gmail.com>
2026-06-26 20:19:28 +0530

    Fix password for mysql

--- db/src/main/java/com/funfriday/db/MySqlConnectionProvider.java
+++ db/src/main/java/com/funfriday/db/MySqlConnectionProvider.java
@@ -7,9 +7,9 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class MySqlConnectionProvider implements DatabaseConnectionProvider {
-    private static final String DEFAULT_URL = "jdbc:mysql://localhost:3306/GameData";
-    private static final String DEFAULT_USERNAME = "root";
-    private static final String DEFAULT_PASSWORD = "";
+    private static final String DEFAULT_URL = "jdbc:mysql://127.0.0.1:3306/GameData";
+    private static final String DEFAULT_USERNAME = "funfriday_app";
+    private static final String DEFAULT_PASSWORD = "funfriday_pass";
     private static final int DEFAULT_MAX_POOL_SIZE = 10;
 
     private final HikariDataSource dataSource;
